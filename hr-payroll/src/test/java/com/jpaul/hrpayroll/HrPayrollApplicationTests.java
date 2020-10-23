4def3671562ffb57200da960d703ac99b755b8d5
João Vitor Paul <joaovitor.paul@gmail.com>
2020-10-22 22:30:20 -0300

    Project hr-payroll created

---
+++ hr-payroll/src/test/java/com/jpaul/hrpayroll/HrPayrollApplicationTests.java
@@ -0,0 +1,11 @@
+package com.jpaul.hrpayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest class HrPayrollApplicationTests {
+
+	@Test void contextLoads() {
+	}
+
+}
